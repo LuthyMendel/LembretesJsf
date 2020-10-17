03a706250197cccfdc774599401d195cc2685e69
LuthyMendel <giom.01@hotmail.com>
2020-10-16 22:38:34 -0300

    Classe manipulação List

---
+++ src/main/java/br/com/lembrete/web/LembreteMapper.java
@@ -0,0 +1,81 @@
+package br.com.lembrete.web;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import br.com.lembrete.Lembrete;
+
+public class LembreteMapper {
+
+	private List<Lembrete> lembretes = new ArrayList<Lembrete>();
+
+	public List<Lembrete> getLembretes() {
+
+		return lembretes;
+	}
+
+	public void adicionar(Lembrete lembrete) {
+
+		// Data Criação
+		Instant agora = LocalDate.now().atStartOfDay().atZone(ZoneId.systemDefault()).toInstant();
+		Date hoje = Date.from(agora);
+		lembrete.setDataCadastro(hoje);
+
+		lembretes.add(lembrete);
+	}
+
+	public void remover(Lembrete lembrete) {
+
+		try {
+
+			lembrete = buscar(lembrete.getId());
+
+			lembretes.remove(lembrete);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+	public void editar(Lembrete lembrete) {
+
+		try {
+			for (Lembrete it : lembretes) {
+
+				if (it.getId() == lembrete.getId()) {
+					it = lembrete;
+					break;
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Lembrete buscar(long id) {
+		Lembrete lembrete = null;
+
+		try {
+			for (Lembrete it : lembretes) {
+				if (it.getId() == id) {
+
+					lembrete = it;
+					break;
+				}
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return lembrete;
+	}
+
+}
