d408622d3900f31739df7c8f54a0ab708c7c73e6
LuthyMendel <giom.01@hotmail.com>
2020-10-17 00:08:49 -0300

    Inserindo lembre sem utilizar injeção de dependência - apenas teste

--- src/main/java/br/com/lembrete/bean/lembreteBean.java
+++ src/main/java/br/com/lembrete/bean/lembreteBean.java
@@ -2,18 +2,30 @@ package br.com.lembrete.bean;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.lembrete.Lembrete;
 import br.com.lembrete.web.LembreteMapper;
+
 
 @ManagedBean
 @SessionScoped
 public class lembreteBean {
 	
+	
 	private Lembrete lembrete;
 	private LembreteMapper mapper = new LembreteMapper();
+	
+	//Não recomendado apenas para teste
+	@PostConstruct
+	public void init() {
+		
+		lembrete = new Lembrete();
+	}
 	
 	public Lembrete getLembrete() {
 		
@@ -26,5 +38,19 @@ public class lembreteBean {
 		return mapper.getLembretes();
 	}
 	
+	public String adicionar() {
+		
+		mapper.adicionar(lembrete);
+		
+		//É preciso reiniciar pois os dados estão dentro da sessão
+		lembrete = new Lembrete();
+		
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.addMessage(null,new FacesMessage("Lembrete adicionando com sucesso"));
+		context.getExternalContext().getFlash().setKeepMessages(true);
+		
+		return "index.xhtml";
+		
+	}
 
 }
